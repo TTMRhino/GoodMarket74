0dc9a2f37065de8bbc69971e9b3a46058f8bfc5a
Morozov Dmitriy Viktorovich <DMorozov@gksm.local>
2020-07-14 14:39:27 +0500

    fix webpack ((

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './app.css';
 
-const App = () =>{
-    return <div>App</div>
+
+const App = () => {
+    return <div > App </div>
 };
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,59 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+    mode: "development",
     module: {
         rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: [{
-                loader: 'babel-loader'
-            }]
-        }]
-    }
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader'
+                }]
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'images/',
+                        // name: '[name]-[sha1:hash:7].[ext]'
+                    }
+                }]
+            },
+
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'fonts/',
+                    }
+                }]
+            },
+            /* {
+                 test: /\.js$/,
+                 use: [{
+                     loader: 'file-loader',
+                     options: {
+                         outputPath: 'js/',
+
+                     }
+                 }]
+             },*/
+            {
+                test: /\.(css)$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            },
+
+        ]
+    },
+
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public//index.html'
+        })
+    ]
 };
